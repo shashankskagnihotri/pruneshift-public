62d6625cfda6f6a2d83c68072204740fbfd94186
agnihotr <agnihotr@cs.uni-freiburg.de>
2021-02-11 17:58:23 +0100

    working KD

---
+++ src/pruneshift/for_graphs.py
@@ -0,0 +1,53 @@
+import csv
+import os
+
+
+#dir = "/misc/student/agnihotr/master_thesis/"
+list1 = ["/misc/lmbraid19/agnihotr/thesis_pruneshift/2021-02-08/22-15-35/metrics.csv",
+	"/misc/lmbraid19/agnihotr/thesis_pruneshift/l1_1.1_augmix/metrics.csv",
+	"/misc/lmbraid19/agnihotr/thesis_pruneshift/l1_1.3_augmix/metrics.csv",
+	"/misc/lmbraid19/agnihotr/thesis_pruneshift/l1_1.5_augmix/metrics.csv",
+	"/misc/lmbraid19/agnihotr/thesis_pruneshift/l1_2_augmix/metrics.csv",
+	"/misc/lmbraid19/agnihotr/thesis_pruneshift/l1_4_augmix/metrics.csv",
+	"/misc/lmbraid19/agnihotr/thesis_pruneshift/l1_8_augmix/metrics.csv",
+	"/misc/lmbraid19/agnihotr/thesis_pruneshift/l1_16_augmix/metrics.csv",
+	"/misc/lmbraid19/agnihotr/thesis_pruneshift/2021-02-08/22-15-35/metrics.csv",
+	"/misc/lmbraid19/agnihotr/thesis_pruneshift/l1_1.1_kd_augmix/metrics.csv",
+	"/misc/lmbraid19/agnihotr/thesis_pruneshift/l1_1.3_kd_augmix/metrics.csv",
+	"/misc/lmbraid19/agnihotr/thesis_pruneshift/l1_1.5_kd_augmix/metrics.csv",
+	"/misc/lmbraid19/agnihotr/thesis_pruneshift/l1_2_kd_augmix/metrics.csv",
+	"/misc/lmbraid19/agnihotr/thesis_pruneshift/l1_4_kd_augmix/metrics.csv",
+	"/misc/lmbraid19/agnihotr/thesis_pruneshift/l1_8_kd_augmix/metrics.csv",
+	"/misc/lmbraid19/agnihotr/thesis_pruneshift/l1_16_kd_augmix/metrics.csv"]
+
+dire="/misc/lmbraid19/agnihotr/shashank_graphs"	
+list2 = ["prune_0",
+	"l1_1.1_augmix",
+	"l1_1.3_augmix",
+	"l1_1.5_augmix",
+	"l1_2_augmix",
+	"l1_4.1_augmix",
+	"l1_8_augmix",
+	"l1_16_augmix",
+	"prune_0",
+	"l1_1.1_kd_augmix",
+	"l1_1.3_kd_augmix",
+	"l1_1.5_kd_augmix",
+	"l1_2_kd_augmix",
+	"l1_4_kd_augmix",
+	"l1_8_kd_augmix",
+	"l1_16_kd_augmix"]
+
+for i in range(len(list1)):
+    f1 = open(list1[i], 'r')
+    path = os.path.join(dire, list2[i])
+    if not os.path.isdir(path):
+    	os.makedirs(path)
+    print(path)
+    filename = path + "/metric.csv"
+    print(filename)
+    with open(filename, 'w') as f2:
+    	#writer = csv.writer(f2)
+    	for line in f1:
+    	    #writer.writerows(line)
+    	    f2.write(line)
--- src/pruneshift/losses.py
+++ src/pruneshift/losses.py
@@ -4,9 +4,9 @@ import torch.nn as nn
 import torch.nn.functional as F
 from pytorch_lightning.metrics.functional import accuracy
 
-from ..distiller_zoo import DistillKL
-from ..crd.criterion import CRDLoss
-
+from .distiller_zoo import DistillKL
+from .crd.criterion import CRDLoss
+from pruneshift import networks
 
 class StandardLoss(nn.Module):
 
@@ -17,18 +17,52 @@ class StandardLoss(nn.Module):
         acc = accuracy(torch.argmax(logits, 1), y)
         return loss, {"acc": acc} 
 
+class CRD_Loss(nn.Module):
+
+    def __init__(self, teacher_path, teacher_model_id, kd_T: float = 4., gamma:float = 0.1, charlie:float = 0.1, delta: float = 0.8):
+        self.teacher_network = create_network(teacher_model_id, ckpt_path=teacher_path)
+        self.kd_T = kd_T 	 	#temperature for KD
+        self.gamma = gamma 	 	#scaling for the classification loss
+        self.charlie = charlie  	#scaling for the KD loss
+        self.delta = delta  	#scaling for the CRD loss
+
+    def forward(self, network: nn.Module, batch):
+        x,y, index, contrast_idx = batch
+        criterion_dv = DistillKL(self.kd_T)
+        
+        feat_s, logit_s = network(x, is_feat=True, preact=preact)
+        self.teacher_network.eval()
+        with torch.no_grad():
+            feat_t, logit_t = self.teacher_network(x, is_feat_True, preact=preact)
+            feat_t = [f.detach() for f in feat_t]
+        f_s = feat_s[-1]
+        f_t = feat_t[-1]
+        s_dim = feat_s[-1].shape[1]
+        t_dim = feat_t[-1].shape[1]
+        n_data = len(x.cpu().detach().numpy())
+        criterion_kd = CRDLoss([s_dim, t_dim, n_data, ])
+        loss_crd = criterion_kd(f_s, f_t, index, constract_idx) * self.delta
+        loss = F.cross_entropy(logits, y) * self.gamma
+        loss_kd = criterion_dv(logits, teacher_logits) * self.charlie
+        acc = accuracy(torch.argmax(logits, 1), y)
+        stats = {"acc": acc, "kl_loss": loss, "KD_loss": loss_kd, "CRD_loss": loss_crd}
+        return loss+loss_kd+loss_crd , stats
+
 class KnowledgeDistill(nn.Module):
 
     def __init__(self, teacher_path, teacher_model_id, kd_T: float = 4., gamma:float = 0.1, charlie:float = 0.9):
-        self.teacher_network = create_network(teacher_model_id, ckpt_path=teacher_path)
+        self.teacher_network = networks.create_network(teacher_model_id, ckpt_path=teacher_path)
         self.kd_T = kd_T 	 	#temperature for KD
         self.gamma = gamma 	 	#scaling for the classification loss
         self.charlie = charlie  	#scaling for the KD loss
 
     def forward(self, network: nn.Module, batch):
         x,y = batch
-        criterion_dv = DistillKL(self.kd_T)
+        
         logits = network(x)
+        #print("\n\n\n\n")
+        #print(logits)
+        criterion_dv = DistillKL(self.kd_T)
         self.teacher_network.eval()
         with torch.no_grad():
             teacher_logits = self.teacher_network(x)
@@ -40,16 +74,16 @@ class KnowledgeDistill(nn.Module):
 
 class Augmix_KnowledgeDistill(nn.Module):
 
-    def __init__(self, teacher_path, teacher_model_id, kd_T: float = 4., charlie:float = 0.9, self, alpha: float = 12., beta: float = 1.):
+    def __init__(self, teacher_path, teacher_model_id, kd_T: float = 4., charlie:float = 0.9, alpha: float = 12., beta: float = 1.):
         """ Implements the AugmixLoss from the augmix paper.
 
         Args:
             alpha: Multiplitave factor for the jensen-shannon divergence.
         """ 
-        super(AugmixLoss, self).__init__()
+        super(Augmix_KnowledgeDistill, self).__init__()
         self.alpha = alpha		#scaling for the augmix loss
         self.beta = beta 		#scaling for the classification loss
-        self.teacher_network = create_network(teacher_model_id, ckpt_path=teacher_path)
+        self.teacher_network = networks.create_network(teacher_model_id, ckpt_path=teacher_path)
         self.kd_T = kd_T 	 	#temperature for KD
         self.charlie = charlie  	#scaling for the KD loss
 
@@ -59,11 +93,12 @@ class Augmix_KnowledgeDistill(nn.Module):
         self.teacher_network.eval()
         criterion_dv = DistillKL(self.kd_T)
         
-        logits = torch.split(network(torch.cat(x)), x[0].shape[0])
+        kd_logits = network(torch.cat(x))        
         with torch.no_grad():
-            teacher_logits = torch.split(self.teacher_network(torch.cat(x)), x[0].shape[0])
+            teacher_logits = self.teacher_network(torch.cat(x))
         
-        loss_kd = criterion_dv(logits, teacher_logits) * self.charlie
+        loss_kd = criterion_dv(kd_logits, teacher_logits) * self.charlie
+        logits = torch.split(kd_logits, x[0].shape[0])
     
         p_clean, p_aug1, p_aug2 = F.softmax(
             logits[0], dim=1), F.softmax(
