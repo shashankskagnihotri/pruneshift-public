1b200ac5f67b03900f5e9362b18debdffb20cf20
Jasper Hoffmann <jasperhoffmann@posteo.de>
2021-02-27 17:57:58 +0100

    Reversed the change in the resnet

--- src/models/resnet.py
+++ src/models/resnet.py
@@ -280,7 +280,6 @@ class ResNet(nn.Module):
             is_feat = True
         else:            
             is_feat = False
-        is_feat = False
         preact = False
         #print(x)
         x = self.conv1(x)
